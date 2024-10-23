94a00b75bdd80fb3bdcf5e5235231c8f5d6696e8
taiyo-ishiyama <taiyou04142001@gmail.com>
2024-10-24 01:38:03 +1000

    Update UI

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,22 +1,21 @@
 import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
-import ActivityList from "./dashboard/ActivityList";
-import { useStore } from "../../app/stores/store";
+import ActivityList from "./ActivityList";
+import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import LoadingComponent from "../../app/layout/LoadingComponent";
-
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import ActivityFilters from "./ActivityFilters";
 
 export default observer(function ActivityDashboard() {
-
-  const {activityStore} = useStore();
-  const {loadActivities, activityRegistry} = activityStore;
+  const { activityStore } = useStore();
+  const { loadActivities, activityRegistry } = activityStore;
 
   useEffect(() => {
     if (activityRegistry.size === 0) loadActivities();
   }, [activityRegistry.size, loadActivities]);
 
-
-  if (activityStore.loadingInitial)  return <LoadingComponent content="Loading app" />
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content='Loading app' />;
 
   return (
     <Grid>
@@ -24,8 +23,8 @@ export default observer(function ActivityDashboard() {
         <ActivityList />
       </Grid.Column>
       <Grid.Column width='6'>
-        <h2>Activities filters</h2>
+        <ActivityFilters />
       </Grid.Column>
     </Grid>
   );
-})
+});
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -5,6 +5,7 @@ import "./app/layout/styles.css";
 import { store, StoreContext } from "./app/stores/store.ts";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router/Routes.tsx";
+import "react-calendar/dist/Calendar.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
